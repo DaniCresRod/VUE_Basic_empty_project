7d028afef00de76eb9813e601e7daf9349471cf8
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-07-11 11:07:49 +0200

    Proyecto básico VUE ( con test, sin pinia ni ts)

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+//import { RouterLink, RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      Hola soy la cabecera
+    </div>
+  </header>
+
+  <RouterView />
+
+</template>
+
+<style scoped>
+header {
+  max-height: 100vh;
+  border: 1px solid black;
+  display: block;
+  height: 20vh;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    hola soy HomeView
+  </main>
+</template>
+
+<style scoped>
+
+main{
+  border: 1px solid blue;
+  display: block;
+  height: 60vh;
+}
+
+</style>
